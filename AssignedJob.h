bf07cc3719c5d12eff6e4f636060fb02c1924120
abdullah.dar <darabdullah380@gmail.com>
2024-10-16 21:58:35 +0500

    Files updated successfully

---
+++ AssignedJob.h
@@ -0,0 +1,16 @@
+/**
+ * Project Untitled
+ */
+
+#include <string>
+using namespace std;
+
+#ifndef _ASSIGNED JOB_H
+#define _ASSIGNED JOB_H
+
+class AssignedJob {
+public:
+    string assignDate;
+};
+
+#endif //_ASSIGNED JOB_H
